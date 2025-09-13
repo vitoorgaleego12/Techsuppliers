f61bdd6c209a7127524e0f94f5497ca8ddc6a7e1
vitoorgaleego12 <vitoorgaleego12@GMAIL.COM>
2025-09-13 03:55:41 -0300

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,12 @@ import logging
 
 from flask import Flask, send_from_directory
 
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)  # isso libera acesso a recursos externos (imagens, APIs, etc.)
+
+
 app = Flask(__name__, static_folder=".", static_url_path="")
 
 # rota para servir qualquer arquivo estático (html, css, js, imagens)
